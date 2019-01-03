fbc0fd486886076994101d5ed91caeaae718f3eb
nullice <ui@nullice.com>
2019-01-03 19:15:34 +0800

    mac-os 模块
    mac-os 模块

--- tool/clean.js
+++ tool/clean.js
@@ -2,19 +2,32 @@ const fsex = require("fs-extra");
 const path = require("path");
 
 console.log("[node_modules]-[windows]" )
-clean(path.join(__dirname,"../windows/node_modules/"))
-console.log("\n\n[node_modules]-[windows]" )
+clean(path.join(__dirname,"../windows/node_modules/"),"win")
+console.log("\n\n[node_modules]-[mac-os]" )
 clean(path.join(__dirname,"../mac-os/node_modules/"))
-function clean(dir) {
+function clean(dir, os) {
 
-   
-    let list = [
-        path.join(dir,"sqlite3","build"),
-        path.join(dir,"sqlite3","deps"),
-        path.join(dir,"sharp","src"),
-        path.join(dir,"sharp","vendor"),
-        
-    ]
+    let list
+    if(os === "win")
+    {
+         list = [
+            path.join(dir,"sqlite3","build"),
+            path.join(dir,"sqlite3","deps"),
+            path.join(dir,"sharp","src"),
+            path.join(dir,"sharp","vendor"),
+        ]
+    }else
+    {
+         list = [
+            path.join(dir,"sqlite3","build"),
+            path.join(dir,"sqlite3","deps"),
+            path.join(dir,"sharp","src"),
+            path.join(dir,"sharp","vendor","include"),
+        ]
+    }
+
+
+
 
     list.forEach(x=>{
         try {
